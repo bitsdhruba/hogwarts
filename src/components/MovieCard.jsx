836f42ef496091538fc026b8f5eca727d0747e62
bitsdhruba <142327840+bitsdhruba@users.noreply.github.com>
2024-06-01 17:15:29 +0530

    movies page rendered.

---
+++ src/components/MovieCard.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+
+const MovieCard = ({ movie }) => {
+  return (
+    <div className="w-[17.5rem] h-[24rem] rounded-lg border border-[#9d0208] shadow-md overflow-hidden">
+      <div className="relative bg-gradient-to-t from-[#10121d] from-20%">
+        <img
+          src={movie?.attributes.poster}
+          alt={movie?.attributes.title}
+          className="w-full inset-2 bg-cover bg-center"
+        />
+        <div className="h-[10rem] min-w-full absolute bottom-0 left-0 px-2 bg-gradient-to-t from-black from-30% flex items-end">
+          <h1 className="text-2xl m-4">{movie?.attributes.title}</h1>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default MovieCard;
--- src/pages/Movies.jsx
+++ src/pages/Movies.jsx
@@ -2,6 +2,8 @@ import axios from "axios";
 import React, { useEffect, useState } from "react";
 import { useLocation } from "react-router-dom";
 import { url } from "../api/api";
+import m_bg from "../assets/harry_movies_bg1.svg";
+import MovieCard from "../components/MovieCard";
 
 const Movies = () => {
   const location = useLocation();
@@ -25,9 +27,24 @@ const Movies = () => {
   console.log(collection);
 
   return (
-    <div className="max-w-[1200px]">
-      <h1>Here is the movies collection.</h1>
-      <div>here is the collection. voilah.</div>
+    <div>
+      <div
+        className="min-w-full h-[40vh] bg-cover bg-center relative"
+        style={{ backgroundImage: `url(${m_bg})` }}
+      >
+        <div className="absolute min-w-full bottom-0 left-0 h-[20vh] bg-gradient-to-t from-black flex items-center justify-center">
+          <h1 className="text-4xl text-white capitalize">
+            Here is the movies collection.
+          </h1>
+        </div>
+      </div>
+      <div className="max-w-[1200px] mx-auto">
+        <div className="flex-wrap flex gap-6 justify-between">
+          {collection?.map((movie) => {
+            return <MovieCard key={movie.id} movie={movie} />;
+          })}
+        </div>
+      </div>
     </div>
   );
 };
